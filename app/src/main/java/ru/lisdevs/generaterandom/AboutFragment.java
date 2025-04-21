0334293fcb3141a0d3e7267621383ffcdf2c919f
Сиденёв Кирилл <wwwkom780@gmail.com>
2025-04-21 21:28:58 +0500

    Initial commit

---
+++ app/src/main/java/ru/lisdevs/generaterandom/AboutFragment.java
@@ -0,0 +1,71 @@
+package ru.lisdevs.generaterandom;
+
+import android.app.Dialog;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.palette.graphics.Palette;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.CustomTarget;
+import com.bumptech.glide.request.transition.Transition;
+
+
+public class AboutFragment extends Fragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_about, container, false);
+
+        LinearLayout tg = (LinearLayout) root.findViewById(R.id.tvTg);
+        LinearLayout vk = (LinearLayout) root.findViewById(R.id.vk);
+
+        tg.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("https://t.me/railcinec"));
+                startActivity(intent);
+            }
+        });
+
+        vk.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("https://vk.com/club223400185"));
+                startActivity(intent);
+            }
+        });
+
+        return root;
+    }
+
+
+}
---
+++ app/src/main/java/ru/lisdevs/generaterandom/MainActivity.java
@@ -0,0 +1,59 @@
+package ru.lisdevs.generaterandom;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ColorGeneratorFragment colorGeneratorFragment;
+    private HomeFragment homeFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        colorGeneratorFragment = new ColorGeneratorFragment();
+        homeFragment = new HomeFragment();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                item -> {
+                    switch (item.getItemId()) {
+                        case R.id.item_generated_color:
+                            changeFragment(new HomeFragment());
+                            break;
+                        case R.id.item_generated_color_image:
+                            changeFragment(new ColorGeneratorFragment());
+                            break;
+                    }
+                    return true;
+                });
+
+        changeFragment(new HomeFragment());
+    }
+
+    private void changeFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.container, fragment)
+                .commit();
+
+    }
+
+    public void onSettings(View view) {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+    }
+
+    public void onAbout(View view) {
+        Intent intent = new Intent(this, AboutActivity.class);
+        startActivity(intent);
+    }
+}
